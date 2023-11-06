ee5cd0ca6620230f1970e182499ebcf0d2d56aa5
Benjamin Caillet <benjamin.caillet@master.hes-so.ch>
2023-11-06 18:14:28 +0100

    Format timeserie.py + add display by gender

--- TimeSeries/timeSeries.py
+++ TimeSeries/timeSeries.py
@@ -2,7 +2,13 @@ from pyaxis import pyaxis
 import numpy as np
 import matplotlib.pyplot as plt
 
-partei_list = ["PLR","PDC","PS","UDC","PVL","PES"]
+party_list = ["PLR","PDC","PS","UDC","PVL","PES"]
+
+party_list_complet = [
+    "PLR", "PDC", "PS", "UDC", "PL", "AdI", "PEV",
+    "PCS", "PVL", "PBD", "PST", "PSA", "POCH", "PES", "AVF", "Sol.",
+    "Rép.", "DS", "UDF", "PSL", "Lega", "MCR", "Sép.", "Autres"
+]
 
 french_to_german = {
     "Partis - Total": "Parteien - Total",
@@ -32,9 +38,9 @@ french_to_german = {
     "Autres": "Übrige"
 }
 
-def plot_data(data_df,selected_partei, kanton):
+def plot_party(data_df,selected_party, canton):
     # Data filtering
-    elect = data_df[(data_df['Partei'].isin(selected_partei)) & (data_df['Ergebnisse'] == 'Gewählte') & (data_df['Kanton'] == kanton) & (data_df['Geschlecht'] == "Geschlecht - Total")]
+    elect = data_df[(data_df['Partei'].isin(selected_party)) & (data_df['Ergebnisse'] == 'Gewählte') & (data_df['Kanton'] == canton) & (data_df['Geschlecht'] == "Geschlecht - Total")]
     elect_filtered = elect
     elect_filtered['DATA'] = np.where(~elect_filtered['DATA'].str.contains(r'\d'), '0', elect_filtered['DATA']) # Filter empty values
     elect_filtered['DATA'] = elect_filtered['DATA'].astype(int)
@@ -43,9 +49,27 @@ def plot_data(data_df,selected_partei, kanton):
     # Plotting
     fig, ax = plt.subplots(figsize=(10, 6))
     pivot_data.plot(kind='line', ax=ax)
-    ax.set_title('Elected Men and Women by Year ' + ' in ' + kanton)
-    ax.set_ylabel('Number Elected')
-    ax.set_xlabel('Year')
-    ax.legend(title="Geschlecht")
+    ax.set_title("Nombre d'élus par année " + 'pour ' + canton)
+    ax.set_ylabel("Nombre d'élus")
+    ax.set_xlabel('Années')
+    ax.legend(title="Elus")
+    
+    return fig
+
+def plot_gender(data_df, canton):
+    # Data filtering
+    elect = data_df[(data_df['Partei'] == 'Parteien - Total') & (data_df['Ergebnisse'] == 'Gewählte') & (data_df['Kanton'] == canton)]
+    elect_filtered = elect[elect['Geschlecht'].isin(['Mann', 'Frau'])]
+    elect_filtered['DATA'] = np.where(~elect_filtered['DATA'].str.contains(r'\d'), '0', elect_filtered['DATA']) # Filter empty values
+    elect_filtered['DATA'] = elect_filtered['DATA'].astype(int)
+    pivot_data = elect_filtered.pivot(index='Jahr', columns='Geschlecht', values='DATA')
+    
+    # Plotting
+    fig, ax = plt.subplots(figsize=(10, 6))
+    pivot_data.plot(kind='line', ax=ax)
+    ax.set_title("Nombre d'élus par année " + 'pour ' +  canton)
+    ax.set_ylabel("Nombre d'élus")
+    ax.set_xlabel('Années')
+    ax.legend(title="Elus")
     
     return fig
--- TimeSeries/timeSeriesTest.py
+++ TimeSeries/timeSeriesTest.py
@@ -1,10 +1,9 @@
 from pyaxis import pyaxis
-import numpy as np
 import matplotlib.pyplot as plt
 import customtkinter as tk
 import customtkinter
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from timeSeries import plot_data, partei_list, french_to_german
+from timeSeries import plot_party, plot_gender, party_list, party_list_complet, french_to_german
 
 # Setting up theme of the app
 customtkinter.set_appearance_mode("light")
@@ -14,41 +13,72 @@ fp = r"Dataset/Dataset.px"
 px = pyaxis.parse(uri=fp, encoding='ANSI')
 data_df = px['DATA']
 
-def updatePlot(selected_partei):
+def updatePlot(selected_party):
     
-    translated_list = [french_to_german[partei] for partei in selected_partei]
-    kanton = kanton_combobox.get()
-    fig = plot_data(data_df,translated_list, kanton)
+    translated_list = [french_to_german[party] for party in selected_party]
+    canton = canton_combobox.get()
+    
+    if gender_combobox.get() == "Total" :
+        fig = plot_party(data_df,translated_list, canton)
+    else :
+        fig = plot_gender(data_df, canton)
 
     canvas = FigureCanvasTkAgg(fig, master=window)
     canvas_widget = canvas.get_tk_widget()
-    canvas_widget.grid(row=4, column=0, columnspan=20)
+    canvas_widget.grid(row=5, column=0, columnspan=20)
     print(translated_list)
     canvas.draw()
     
 def open_selection_popup():
     # Create a popup
     popup = tk.CTkToplevel(window)
-    popup.title("Select partei")
-    
+    popup.title("Select Party")
+
+    # Set a fixed height
+    popup_height = 400
+    popup.geometry(f'200x{popup_height}')
+
+    # Create a container frame
+    container = tk.CTkFrame(popup)
+    container.pack(fill='both', expand=True)
+
+    # Create a canvas and attach a scrollbar to it
+    canvas = tk.CTkCanvas(container)
+    scrollbar = tk.CTkScrollbar(container, orientation="vertical", command=canvas.yview)
+    canvas.configure(yscrollcommand=scrollbar.set)
+
+    # Pack the scrollbar to the right and fill in the Y direction
+    scrollbar.pack(side="right", fill="y")
+
+    # Pack the canvas to expand and fill in both directions
+    canvas.pack(side="left", fill="both", expand=True)
+
+    # Create a frame inside the canvas to hold the widgets
+    checkbox_frame = tk.CTkFrame(canvas)
+    canvas.create_window((0, 0), window=checkbox_frame, anchor="nw")
+
     # Variables to store checkbox states
-    vars_ = [tk.BooleanVar() for _ in partei_list]
-    
+    vars_ = [tk.BooleanVar() for _ in party_list_complet]
+
     # Create checkboxes for each item
     checkboxes = []
-    for i, item in enumerate(partei_list):
-        cb = tk.CTkCheckBox(popup, text=item, variable=vars_[i])
+    for i, item in enumerate(party_list_complet):
+        cb = tk.CTkCheckBox(checkbox_frame, text=item, variable=vars_[i])
         cb.pack(anchor="w", padx=10, pady=5)
         checkboxes.append(cb)
 
-    # OK button to print selected partei and destroy popup
+    # Update the scrollregion of the canvas to encompass the checkbox frame
+    checkbox_frame.update_idletasks()
+    canvas.config(scrollregion=canvas.bbox("all"))
+
+    # OK button to print selected party and destroy popup
     def confirm_selection():
-        selected_partei = [item for i, item in enumerate(partei_list) if vars_[i].get()]
-        updatePlot(selected_partei)
+        selected_party = [item for i, item in enumerate(party_list_complet) if vars_[i].get()]
+        updatePlot(selected_party)
         popup.destroy()
-    
+
     ok_button = tk.CTkButton(popup, text="OK", command=confirm_selection)
-    ok_button.pack(pady=10)
+    ok_button.pack(after=container, anchor="center", pady=10)
     
 def _quit():
     window.quit()
@@ -56,34 +86,37 @@ def _quit():
 
 # GUI setup
 window = tk.CTk()
-window.title("Partei and Kanton Selection")
+window.title("Sélection du parti et du canton")
 
 # Button to open the popup
-btn = tk.CTkButton(window, text="Select partei", command=open_selection_popup)
+btn = tk.CTkButton(window, text="Sélection des partis", command=open_selection_popup)
 btn.grid(row=0, column=0, padx=0, pady=20)
 
-# Partei dropdown
-partei_label = tk.CTkLabel(window, text="Parti:")
-partei_label.grid(row=1, column=0, padx=0, pady=20)
-partei_combobox = tk.CTkComboBox(window, values=partei_list)
-partei_combobox.grid(row=1, column=1, padx=0, pady=20)
-partei_combobox.set("UDC")
+selected_party = party_list
 
-selected_partei = partei_list
+# Canton dropdown
+canton_label = tk.CTkLabel(window, text="Canton:")
+canton_label.grid(row=1, column=0, padx=0, pady=20)
+canton_list = ["Schweiz","Zürich","Bern / Berne","Luzern","Uri","Schwyz","Obwalden","Nidwalden","Glarus","Zug","Fribourg / Freiburg","Solothurn","Basel-Stadt","Basel-Landschaft","Schaffhausen","Appenzell Ausserrhoden","Appenzell Innerrhoden","St. Gallen","Graubünden / Grigioni / Grischun","Aargau","Thurgau","Ticino","Vaud","Valais / Wallis","Neuchâtel","Genève","Jura"]
+canton_combobox = tk.CTkComboBox(window, values=canton_list,state="readonly")
+canton_combobox.grid(row=1, column=1, padx=0, pady=20)
+canton_combobox.set("Schweiz")
 
-# Kanton dropdown
-kanton_label = tk.CTkLabel(window, text="Canton:")
-kanton_label.grid(row=2, column=0, padx=0, pady=20)
-kanton_list = ["Schweiz","Zürich","Bern / Berne","Luzern","Uri","Schwyz","Obwalden","Nidwalden","Glarus","Zug","Fribourg / Freiburg","Solothurn","Basel-Stadt","Basel-Landschaft","Schaffhausen","Appenzell Ausserrhoden","Appenzell Innerrhoden","St. Gallen","Graubünden / Grigioni / Grischun","Aargau","Thurgau","Ticino","Vaud","Valais / Wallis","Neuchâtel","Genève","Jura"]
-kanton_combobox = tk.CTkComboBox(window, values=kanton_list)
-kanton_combobox.grid(row=2, column=1, padx=0, pady=20)
-kanton_combobox.set("Schweiz")
+# Gender dropdown
+gender_label = tk.CTkLabel(window, text="Genre:")
+gender_label.grid(row=2, column=0, padx=0, pady=20)
+gender_list = ["Total","Homme/Femme"]
+gender_combobox = tk.CTkComboBox(window, values=gender_list,state="readonly")
+gender_combobox.grid(row=2, column=1, padx=0, pady=20)
+gender_combobox.set("Total")
+
+
 
 # Plot button
-plot_button = tk.CTkButton(window, text="Plot", command=updatePlot)
+plot_button = tk.CTkButton(window, text="Plot", command=lambda: updatePlot(selected_party))
 plot_button.grid(row=3, column=0, columnspan=2, pady=20)
 
-updatePlot(partei_list)
+updatePlot(selected_party)
 
 window.protocol("WM_DELETE_WINDOW", _quit)
 window.mainloop()
