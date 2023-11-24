02173b84815c919f6aab9b731310ece0310ba99f
mathieu schupbach <mathieu.schupbach@gmail.com>
2023-11-24 10:07:23 +0100

    udate map : test et legende

--- Map/map.py
+++ Map/map.py
@@ -2,10 +2,12 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from matplotlib.colors import TwoSlopeNorm, LinearSegmentedColormap, ListedColormap
 import numpy as np
+from matplotlib.ticker import MultipleLocator
 from pyaxis import pyaxis
 import pandas as pd
 
-from Map.info import dictionary_canton_to_french, colors_from_french_party,french_to_german_party
+from info import dictionary_canton_to_french, colors_from_french_party,french_to_german_party
+
 
 def creat_geoswitz(path):
     #download the map
@@ -52,33 +54,40 @@ def plot_num(switzerland,mode=0,title='Carte de la Suisse'):
     sorted_data = sorted(switzerland['DATA'])
     #define the size of plot
     fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-    # Create a custom colormap with just two colors: red and blue
-    cmap_colors = ['#FF0000','#FFFFFF', '#00FF00']
-    custom_cmap = LinearSegmentedColormap.from_list('CustomRedToGreen', cmap_colors, N=256)
-    vmin=switzerland['DATA'].min()
-    vmax=switzerland['DATA'].max()
-    if abs(vmin)>abs(vmax):
-        vmax=abs(vmin)
+    # normalistion legende
+    vmin = switzerland['DATA'].min()
+    vmax = switzerland['DATA'].max()
+    if abs(vmin) > abs(vmax):
+        vmax = abs(vmin)
     else:
         vmin = -vmax
-    norm = TwoSlopeNorm(vmin=vmin,vcenter=0, vmax=vmax)
+    norm = TwoSlopeNorm(vmin=vmin - 0.5, vcenter=0, vmax=vmax + 0.5)
+
+    # Create a custom colormap with just two colors: red and blue
+    cmap_colors = ['#FF0000', '#FFFFFF', '#00FF00']
+    custom_cmap = LinearSegmentedColormap.from_list('CustomRedToGreen', cmap_colors, N=(vmax - vmin) + 1)
 
     #plot the map whit color blue to red
     if mode == 0:
-        switzerland.plot(column='DATA', cmap=custom_cmap,ax=ax, legend=True,norm=norm)
+        sw=switzerland.plot(column='DATA', cmap=custom_cmap,ax=ax, legend=False,norm=norm)
     if mode == 1:
-        switzerland.plot(column='DATA', cmap="RdBu_r", ax=ax, legend=True,norm=norm)
+        sw=switzerland.plot(column='DATA', cmap="RdBu_r", ax=ax, legend=False,norm=norm)
     #add a limite of canton
     switzerland.boundary.plot(ax=ax, color='black', linewidth=0.5)
     # add legend
     plt.axis('off')
     plt.title(title)
+    # Customize the colorbar ticks with integers
+    ticks = np.arange(vmin, vmax + 1, 1)
+    cbar = plt.colorbar(sw.get_children()[0], ax=ax, ticks=ticks, orientation='vertical')
     return fig
 
 def plot_obj(switzerland,name_data="DATA",title='Carte de la Suisse'):
     # define the size of plot
     fig, ax = plt.subplots(1, 1, figsize=(10, 10))
     # plot the map
+    #switzerland.plot(column=name_data, ax=ax, cmap="viridis", legend=True)
+
     cmap = ListedColormap([colors_from_french_party[legend] for legend in switzerland["Partis"]])
     switzerland.plot(column=name_data,ax=ax, cmap=cmap, legend=True)
 
@@ -89,6 +98,7 @@ def plot_obj(switzerland,name_data="DATA",title='Carte de la Suisse'):
         handle = plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10)
         legend_handles.append((handle, legend))
     ax.legend(*zip(*legend_handles), title='Légendes', loc='upper right')
+
     # add a limite of canton
     switzerland.boundary.plot(ax=ax, color='black', linewidth=0.5)
     # add legend
@@ -121,9 +131,13 @@ def plot_nb_elu_party(dataset,party,year_old,year_new):
 
     subset_com["DATA"]=data_com
     subset_com["Kanton"] = transormation_vo_French(subset_com["Kanton"])
-    switzerland = creat_geoswitz('Dataset/Swiss.geojson')
+    switzerland = creat_geoswitz('mapSwiss.geojson')
     switzerland_data = fusion_data_map(switzerland, subset_com)
-    fig=plot_num(switzerland_data,0,'Party')
+
+    trad_german_to_french = {v: k for k, v in french_to_german_party.items()}
+    party=trad_german_to_french[party]
+    name="Evolution du "+party+" entre "+year_old+" et "+year_new
+    fig=plot_num(switzerland_data,0,name)
     return fig
 
 def plot_diff_gender(dataset,gender,year_old,year_new):
@@ -152,9 +166,14 @@ def plot_diff_gender(dataset,gender,year_old,year_new):
 
     subset_com["DATA"]=data_com
     subset_com["Kanton"] = transormation_vo_French(subset_com["Kanton"])
-    switzerland = creat_geoswitz('Dataset/Swiss.geojson')
+    switzerland = creat_geoswitz('mapSwiss.geojson')
     switzerland_data = fusion_data_map(switzerland, subset_com)
-    fig=plot_num(switzerland_data,0,'Genre')
+    if gender == "Mann":
+        n1="Evolution de la représentation masculine"
+    else:
+        n1="Evolution de la représentation femminine"
+    name = n1 + " entre " + year_old + " et " + year_new
+    fig=plot_num(switzerland_data,0,name)
     return fig
 def plot_best_party(dataset,year):
     mask = dataset["Jahr"] == year
@@ -176,12 +195,16 @@ def plot_best_party(dataset,year):
         data = data.fillna(0).astype(int)
         indice_max = data.idxmax()
         mini_tables[i] = mini_table.loc[indice_max]
+
     df_best_Partei_by_kanton=pd.DataFrame(mini_tables)
+
     df_best_Partei_by_kanton["Kanton"] = transormation_vo_French(df_best_Partei_by_kanton["Kanton"])
-    switzerland = creat_geoswitz('Dataset/swiss.geojson')
+
+    switzerland = creat_geoswitz('mapSwiss.geojson')
     switzerland_data = fusion_data_map(switzerland, df_best_Partei_by_kanton,1)
     trad_german_to_french = {v: k for k, v in french_to_german_party.items()}
     switzerland_data["Partis"] = switzerland_data["Partei"].map(trad_german_to_french)
     switzerland_data["Couleur"] = switzerland_data["Partis"].map(colors_from_french_party)
-    fig=plot_obj(switzerland_data,"Partis")
+    name =  "Meilleur partie par canton en " + year
+    fig=plot_obj(switzerland_data,"Partis",name)
     return fig
