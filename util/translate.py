e1157954fe67a77d7742062a946ece032902a354
Ley Klussyn <92080753+LeyKlussyn@users.noreply.github.com>
2023-11-24 09:31:21 +0100

    translation + colors update

--- util/translate.py
+++ util/translate.py
@@ -1,5 +1,13 @@
 
-# Traduction noms de partis
+# GUI translate
+
+# with array: [0=german, 1=french]
+
+parlement_title_in_year = ["Bundesrat im", "Conseil National en"]
+
+
+
+# Party name translation
 
 party_french_to_german = {
     "Partis - Total": "Parteien - Total",
@@ -30,3 +38,22 @@ party_french_to_german = {
 }
 
 party_german_to_french = {v: k for k, v in party_french_to_german.items()}
+
+# Groups
+#  <https://www.bfs.admin.ch/asset/en/27285176> 
+
+
+group_french_to_german = {
+    "PLR": "FDP",
+    "UDC": "SVP",
+    "PS": "SP",
+    "Les Verts": "Grüne",
+    "Verts Libéraux": "Grünliberale",
+    "Le Centre": "Die Mitte",
+    "Divers Centre": "Kleine Mitteparteien",
+    "Divers Gauche": "Kleine Linksparteien",
+    "Divers Droite": "Kleine Rechtsparteien",
+    "Autres": "Übrige"
+}
+
+group_german_to_french = {v: k for k, v in group_french_to_german.items()}
