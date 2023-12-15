b357d8944a90b6a71b89150f16162803f9af1beb
mathieu schupbach <mathieu.schupbach@gmail.com>
2023-12-15 06:57:52 +0100

    update intro2

--- intro2.py
+++ intro2.py
@@ -28,62 +28,16 @@ def create_circle_button_with_image(window, x, y, radius, image_path, text,butto
 
     # Create the circular button
     button = tk.CTkButton(window, image=tk_image, command=lambda :button_click(button_id))
-    button.configure(width=2 * radius, height=2 * radius, border_width=0,corner_radius=0,anchor="w",fg_color="transparent")
+    button.configure(width=0, height=0, border_width=0,corner_radius=0,fg_color="transparent",border_spacing=0)
 
     # Pack the button at the specified position
     button.place(x=x - radius, y=y - radius)
 
     button.bind("<Enter>", lambda event: button_enter(event, button_id))
     button.bind("<Leave>", lambda event: button_leave(event, button_id))
-    button.configure(text="")
-    # Create a label for the text
-    label = tk.CTkLabel(window, text=text)
-    label.place(x=x, y=y + radius+10)
-    return button,label
+    button.configure(text=text,hover=True,compound="top",anchor="S")
 
-
-def on_circle_hover(event):
-    item_id = event.widget.find_closest(event.x, event.y)
-    item_type = event.widget.type(item_id)
-    if (item_type=="oval") or (item_type=="image"):
-        if item_type=="oval":
-            circle_number = int(event.widget.gettags(item_id)[0])  # Get the circle number from tags
-        elif item_type =="image":
-            circle_number = int(event.widget.gettags(item_id)[0])
-            item_id = image_circle_mapping[circle_number]
-        label.configure(text=f"Afficher événement: {events[circle_number-1]}")  # Update the label text
-        #canvas.itemconfig(item_id, fill=events_colors_hover[circle_number-1])
-
-        # Déplacer le texte explicatif à gauche ou à droite
-        x, y, _, _ = event.widget.coords(item_id)
-        h_l=explanation_label.winfo_height()
-        w_l=explanation_label.winfo_width()
-        if x < app.winfo_reqwidth() / 2:
-            explanation_label.place(x=x + (radius+4)*2 +40, y=y+radius-(h_l/2))
-        else:
-            explanation_label.place(x=x -w_l - 40, y=y+radius-(h_l/2))
-
-        for i, tk_image in enumerate(image_list):
-            if i + 1 == circle_number:
-                continue  # Skip the current image
-            app.itemconfig(image_circle_mapping[i + 1], state="hidden")
-
-def on_circle_leave(event):
-    item_id = event.widget.find_closest(event.x, event.y)
-    item_type = event.widget.type(item_id)
-    if (item_type == "oval") or (item_type == "image"):
-        if item_type=="oval":
-            circle_number = int(event.widget.gettags(item_id)[0])  # Get the circle number from tags
-            #canvas.itemconfig(item_id, fill=events_colors[circle_number-1])
-        elif item_type =="image":
-            circle_number = int(event.widget.gettags(item_id)[0])
-            item_id=image_circle_mapping[circle_number]
-            #anvas.itemconfig(item_id, fill=events_colors[circle_number - 1])
-        label.config(text="")  # Clear the label when leaving a circle
-        explanation_label.place_forget()  # Cacher le texte explicatif
-
-        for i, tk_image in enumerate(image_list):
-            app.itemconfig(image_circle_mapping[i + 1], state="normal")
+    return button
 
 def button_click(button_id):
         # Handle the click event - you can customize this part
@@ -91,31 +45,35 @@ def button_click(button_id):
 
 def button_enter(event,button_id):
 
-    label.configure(text=f"Afficher événement: {events[button_id - 1]}")
-    button=button_map[button_id]
+    explanation_label.configure(text=f"Afficher événement: {events[button_id]}")
     # Déplacer le texte explicatif à gauche ou à droite
-    x = button.winfo_x()
-    y = button.winfo_y()
-    h_l = explanation_label.cget("height")
-    w_l = explanation_label.cget("width")
-    if x < app.winfo_reqwidth() / 2:
-        explanation_label.place(x=x + (radius + 4) * 2 + 40, y=y + radius - (h_l / 2))
+    circle_idx = 4 * (row) + (col)
+    row_b=int(button_id/4)
+    col_b=button_id%4
+    x_b = (col_b + 0.5) * circle_spacing_x
+    y_b = (row_b + 0.5) * circle_spacing_y
+    w_b = explanation_label.winfo_reqwidth()
+    if col_b <  2:
+        explanation_label.place(x=x_b+radius+30, y=y_b)
     else:
-        explanation_label.place(x=x - w_l - 40, y=y + radius - (h_l / 2))
-
-    for i, tk_image in enumerate(image_list):
-        if i + 1 == circle_number:
-            continue  # Skip the current image
-        app.itemconfig(image_circle_mapping[i + 1], state="hidden")
-
+        explanation_label.place(x=x_b-30-w_b, y=y_b)
+    for id in range(rows*columns):
+        if id != button_id:
+            button=button_map[id]
+            button.place_forget()
 
 
 
 def button_leave(event,button_id):
-    for id in range(rows*columns):
-        if id != button_id:
-            button=button_map[id]
-            button.place()
+    for row in range(rows):
+        for col in range(columns):
+            x = (col + 0.5) * circle_spacing_x
+            y = (row + 0.5) * circle_spacing_y
+            circle_idx = 4 * (row) + (col)
+            button=button_map[circle_idx]
+            button.place(x=x - radius, y=y - radius)
+    explanation_label.place_forget()
+
 
 
 
@@ -134,17 +92,6 @@ app.geometry("{}x{}".format(windos_w,windos_h))
 image_list=[]
 button_map = {}
 
-# Create a Label at the top
-label = tk.CTkLabel(app, text="", font=("Helvetica", 12),state=tk.DISABLED)
-
-
-
-# Create a Label of circle
-explanation_label = tk.CTkLabel(app, text="C'est un text test pour l'instant", font=("Helvetica", 10),state=tk.DISABLED)
-explanation_label.configure(fg_color="white")
-explanation_label.place_forget()  # Initialiser en tant que caché
-
-
 
 # Calculate the spacing between circles
 circle_spacing_x = windos_w / columns
@@ -159,9 +106,17 @@ for row in range(rows):
         y = (row + 0.5) * circle_spacing_y
         circle_idx = 4*(row) + (col)
         print(circle_idx,row,col)
-        circle_number = row * columns + col + 1
-        button,label_id = create_circle_button_with_image(app, x, y, radius,images_path[circle_idx],events[circle_idx],circle_idx)
+        button= create_circle_button_with_image(app, x, y, radius,images_path[circle_idx],events[circle_idx],circle_idx)
         button_map[circle_idx] = button
+
+# Create a Label of circle
+explanation_label = tk.CTkLabel(app, text="C'est un text test pour l'instant", font=("Helvetica", 10),state="disabled")
+explanation_label.configure(fg_color="white")
+explanation_label.place()
+#explanation_label.place_forget()  # Initialiser en tant que caché
+
+
+
 
 # Start the Tkinter event loop
 app.mainloop()
